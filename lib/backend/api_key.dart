a704a8556cb5067f14ab3b713ac5010de3dccb0c
destroyer123456-dev <129885472+destroyer123456-dev@users.noreply.github.com>
2025-01-26 17:12:14 -0500

    chatbot

---
+++ lib/backend/api_key.dart
@@ -0,0 +1,3 @@
+class ApiKey {
+  static const String openAIApiKey = "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";
+}
