f90b4c1203780d8ccaffc039c847f2f6d9bd587b
CarrotNinja <r-sherwani@outlook.com>
2025-06-11 14:51:42 -0400

    error fixes

--- lib/home_page_with_nav.dart
+++ lib/home_page_with_nav.dart
@@ -28,11 +28,11 @@ class HomePageWithNav extends StatefulWidget {
   @override
   HomePageWithNavState createState() => HomePageWithNavState();
 }
-
 class HomePageWithNavState extends State<HomePageWithNav> {
   int _selectedIndex = 0;
+  bool _isInitialized = false; // Add initialization flag
 
-  static List<Widget> _pages = <Widget>[
+    static List<Widget> _pages = <Widget>[
     HomePage(),
     Transactions(),
     SettingsPage(),
@@ -42,16 +42,27 @@ class HomePageWithNavState extends State<HomePageWithNav> {
     HomePage() // Your 7th page
   ];
 
-  void onItemTapped(int index) {
-    if (index == 3) { // "More" tab selected
-      _showMoreMenu(context);
-      return;
-    }
-    setState(() {
-      _selectedIndex = index;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _isInitialized = true;
+      });
     });
   }
 
+    void onItemTapped(int index) {
+    if (index >= 0 && index < _pages.length) {
+      if (index == 3) { // "More" tab selected
+        _showMoreMenu(context);
+        return;
+      }
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
   void _showMoreMenu(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -96,11 +107,19 @@ class HomePageWithNavState extends State<HomePageWithNav> {
       ),
     );
   }
-
   @override
   Widget build(BuildContext context) {
+    if (!_isInitialized) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+    
     return Scaffold(
-      body: _pages[_selectedIndex],
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
       bottomNavigationBar: CustomNavBar(
         selectedIndex: _selectedIndex,
         onTabChange: onItemTapped,
@@ -108,3 +127,82 @@ class HomePageWithNavState extends State<HomePageWithNav> {
     );
   }
 }
+// class HomePageWithNavState extends State<HomePageWithNav> {
+//   int _selectedIndex = 0;
+
+  // static List<Widget> _pages = <Widget>[
+  //   HomePage(),
+  //   Transactions(),
+  //   SettingsPage(),
+  //   BudgetSavingsPage(),
+  //   Reports(),
+  //   SpendingHabitPage(),
+  //   HomePage() // Your 7th page
+  // ];
+
+//   void onItemTapped(int index) {
+//     if (index == 3) { // "More" tab selected
+//       _showMoreMenu(context);
+//       return;
+//     }
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   void _showMoreMenu(BuildContext context) {
+//     showModalBottomSheet(
+//       context: context,
+//       builder: (context) => Container(
+//         color: Colors.black,
+//         child: Wrap(
+//           children: [
+//             ListTile(
+//               leading: Icon(Icons.savings, color: Colors.white),
+//               title: Text('Savings', style: TextStyle(color: Colors.white)),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 setState(() => _selectedIndex = 3);
+//               },
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.summarize, color: Colors.white),
+//               title: Text('Reports', style: TextStyle(color: Colors.white)),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 setState(() => _selectedIndex = 4);
+//               },
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.query_stats, color: Colors.white),
+//               title: Text('Analysis', style: TextStyle(color: Colors.white)),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 setState(() => _selectedIndex = 5);
+//               },
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.emoji_events, color: Colors.white),
+//               title: Text('Awards', style: TextStyle(color: Colors.white)),
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 setState(() => _selectedIndex = 6);
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _pages[_selectedIndex],
+//       bottomNavigationBar: CustomNavBar(
+//         selectedIndex: _selectedIndex,
+//         onTabChange: onItemTapped,
+//       ),
+//     );
+//   }
+// }
